d1c347c242e865fdf482233303c1c4c15963b2ec
pure <purifiedgit@gmail.com>
2024-08-18 22:06:26 +0200

    Migrate platform checks back into C++ lmao

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,13 @@
+#if defined( __linux__ )
+	#define BP_LINUX 1
+	#define BP_WINDOWS 0
+#elif defined( _WIN32 ) || defined( _WIN64 )
+	#define BP_LINUX 0
+	#define BP_WINDOWS 1
+#else
+	#error Unsupported operating system
+#endif
+
 #if BP_LINUX
 	#include <sys/types.h>
 	#include <sys/socket.h>
